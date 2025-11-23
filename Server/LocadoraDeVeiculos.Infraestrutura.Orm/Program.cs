bdd08bba5d46f85a8a0b1c03c8ea273739dbfb91
davidinovargas <davidinovargas@gmail.com>
2025-11-22 23:45:11 -0300

    Adicionado arquivos base

---
+++ Server/LocadoraDeVeiculos.Infraestrutura.Orm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LocadoraDeVeiculos.Infraestrutura.Orm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Server/LocadoraDeVeiculos.Testes.Unidade/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace LocadoraDeVeiculos.Testes.Unidade
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
