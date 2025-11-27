d4dbb0517ff4a7ed0bf270f1e20dc7e9729358ab
davidinovargas <davidinovargas@gmail.com>
2025-11-27 03:00:51 -0300

    Adicionado Dominio Autenticação

---
+++ Server/LocadoraDeVeiculos.Core.Dominio/ModuloAutenticacao/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Core.Dominio.ModuloAutenticacao
+{
+    public class Usuario : IdentityUser<Guid>
+    {
+        public Usuario()
+        {
+            Id = Guid.NewGuid();
+            EmailConfirmed = true;
+        }
+    }
+}
