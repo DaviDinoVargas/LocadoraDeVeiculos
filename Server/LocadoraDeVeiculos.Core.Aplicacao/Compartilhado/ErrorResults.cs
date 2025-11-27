6f78f8efc67d56d89352cf2fbac132f7a4ff31a5
davidinovargas <davidinovargas@gmail.com>
2025-11-27 03:09:41 -0300

    Adicionado RoleSeeder

---
+++ Server/LocadoraDeVeiculos.Core.Aplicacao/Compartilhado/ErrorResults.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentResults;
+
+namespace LocadoraDeVeiculos.Core.Aplicacao.Compartilhado
+{
+    public abstract class ErrorResults
+    {
+        public static Error BadRequestError(List<string> erros)
+        {
+            return new Error("Requisição mal formatada")
+                .CausedBy(erros)
+                .WithMetadata("ErrorType", "BadRequest");
+        }
+
+        public static Error NotFoundError(Guid id)
+        {
+            return new Error("Registro não encontrado")
+                .CausedBy($"Não foi possível encontrar o registro com o ID {id}")
+                .WithMetadata("ErrorType", "NotFound");
+        }
+
+        public static Error InternalServerError(Exception ex)
+        {
+            return new Error("Erro interno de servidor")
+                .CausedBy(ex)
+                .WithMetadata("ErrorType", "InternalServer");
+        }
+    }
+}
---
+++ Server/LocadoraDeVeiculos.Infraestrutura.Orm/Identify/RoleSeeder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using LocadoraDeVeiculos.Core.Dominio.ModuloAutenticacao;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.Identify
+{
+    public static class RoleSeeder
+    {
+        public static async Task CriarCargosAsync(RoleManager<Cargo> roleManager)
+        {
+            string[] cargos = { "usuario", "empresa", "administrador" };
+
+            foreach (var cargo in cargos)
+            {
+                bool existe = await roleManager.RoleExistsAsync(cargo);
+
+                if (!existe)
+                {
+                    await roleManager.CreateAsync(new Cargo
+                    {
+                        Name = cargo,
+                        NormalizedName = cargo.ToUpper()
+                    });
+                }
+            }
+        }
+    }
+}
