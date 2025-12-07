b948aed9e749475be8c1896950ca800bd8294b01
davidinovargas <davidinovargas@gmail.com>
2025-12-07 10:44:44 -0300

    Adicionado Dominio Funcionario

---
+++ Server/LocadoraDeVeiculos.Core.Dominio/ModuloFuncionario/Funcionario.cs
@@ -0,0 +1,49 @@
+﻿using LocadoraDeVeiculos.Core.Dominio.ModuloAutenticacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Core.Dominio.ModuloFuncionario;
+
+public class Funcionario : EntidadeBase<Funcionario>
+{
+    public string NomeCompleto { get; set; }
+    public string Cpf { get; set; }
+    public string Email { get; set; }
+    public decimal Salario { get; set; }
+    public DateTimeOffset AdmissaoEmUtc { get; set; }
+
+    public Guid UsuarioId { get; set; }
+    public Usuario? Usuario { get; set; }
+
+    protected Funcionario() { }
+
+    public Funcionario(
+        Guid usuarioId,
+        Guid tenantId,
+        string nomeCompleto,
+        string cpf,
+        string email,
+        decimal salario,
+        DateTimeOffset admissaoEmUtc
+    )
+    {
+        UsuarioId = usuarioId;
+        EmpresaId = tenantId;
+        NomeCompleto = nomeCompleto;
+        Cpf = cpf;
+        Email = email;
+        Salario = salario;
+        AdmissaoEmUtc = admissaoEmUtc;
+    }
+
+    public override void AtualizarRegistro(Funcionario registroEditado)
+    {
+        NomeCompleto = registroEditado.NomeCompleto;
+        Cpf = registroEditado.Cpf;
+        Salario = registroEditado.Salario;
+        AdmissaoEmUtc = registroEditado.AdmissaoEmUtc;
+    }
+}
